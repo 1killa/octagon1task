25fca75b224846af32c39bedb9e1a7c1d5ad94ba
Andrew <defibrullator@gmail.com>
2026-07-17 23:33:47 +0800

    add /help, /site, /creator commands

--- bot.js
+++ bot.js
@@ -9,4 +9,23 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(chatId, "Привет, октагон!");
 });
 
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  const text =
+    "Список доступных команд:\n\n" +
+    "/site - отправляет ссылку на сайт Октагона\n" +
+    "/creator - отправляет ФИО создателя бота";
+  bot.sendMessage(chatId, text);
+});
+
+bot.onText(/\/site/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "https://octagon-students.ru/");
+});
+
+bot.onText(/\/creator/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Хусхаев Андрей");
+});
+
 console.log("Бот запущен...");
